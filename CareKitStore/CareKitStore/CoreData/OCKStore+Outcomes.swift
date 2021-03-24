4231e11a7d6876d0cf13f453accdb59ff69902bf
Nizar Allibhoy <nizarallibhoy@gmail.com>
2021-03-23 18:09:16 -0700

    Put defaultSortDescriptors() back into sortDescriptors for outcomes

--- CareKitStore/CareKitStore/CoreData/OCKStore+Outcomes.swift
+++ CareKitStore/CareKitStore/CoreData/OCKStore+Outcomes.swift
@@ -165,6 +165,6 @@ extension OCKStore {
             switch order {
             case .date(let ascending): return NSSortDescriptor(keyPath: \OCKCDOutcome.startDate, ascending: ascending)
             }
-        } + [NSSortDescriptor(keyPath: \OCKCDObject.updatedDate, ascending: true)]
+        } + query.defaultSortDescriptors() + [NSSortDescriptor(keyPath: \OCKCDObject.updatedDate, ascending: true)]
     }
 }
